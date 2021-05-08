66a9c378fd1501853e26259df90db3c6ea2ab7eb
LeanLescano <tatilescano11@gmail.com>
2021-05-08 19:02:20 -0300

    Meta component

---
+++ pages/components/meta.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+
+function Meta() {
+  return (
+    <Head>
+      <title>Peluqueria | Simbiosis</title>
+      <meta
+        name="description"
+        content="Simbois Peluqueria, 20 años de experiencia más la utilizacion de productos de la mejor calidad mundial nos transforman en unos de los centros de estetica mas importantes de la zona."
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
+
+export default Meta;
