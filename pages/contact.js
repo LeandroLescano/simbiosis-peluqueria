cd27a5997ad5ab9ca2dd5c022049ad55b2af50de
Juan <49132815+juanccassano@users.noreply.github.com>
2021-05-10 22:04:23 -0300

    Grid gallery

---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Meta from "./components/meta";
+
+function Contact() {
+  return (
+    <>
+      <Meta />
+      <h1>Contacto</h1>
+      <p> Prueba </p>
+    </>
+  );
+}
+
+export default Contact;
