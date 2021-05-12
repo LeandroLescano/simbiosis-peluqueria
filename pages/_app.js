7a2ecdd9faa42c7b69fae0705f6fe3a53343ca1f
Juan <49132815+juanccassano@users.noreply.github.com>
2021-05-12 18:39:19 -0300

    Galeria sigue sin funcionar je

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "../styles/index.scss";
 import "../styles/media-container.scss";
+/*
 import "../styles/sponsors.scss";
+*/
 
 // This default export is required in a new `pages/_app.js` file.
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,30 @@ import Layout from "../components/layout";
 export default function Home() {
   return (
     <Layout title="Peluqueria | Simbiosis">
-      <main>
-        <h2 className="m-0">Desde 2004</h2>
-        <h1 className="title">Simbiosis</h1>
-        <h2 className="mb-4">peluquería y estética</h2>
-        <p className="fs-4 text-center w-75">
-          <span className="highlight">Simbiosis</span> es un centro de estética
-          unisex supervisado por sus dueños y atendido por estilistas
-          profesionales que utilizan los mejores productos del mercado
-          internacional para el cuidado de tu belleza integral. Acercate o
-          encontranos en nuestras redes sociales para conocer las novedades que
-          nuestro equipo tiene para ofrecerte. Te esperamos!
-        </p>
-        <div className="button-sections d-none d-md-block">
-          <button className="draw meet">Servicios</button>
-          <Link href="/contact">
-            <button className="draw meet">Contacto</button>
-          </Link>
-          <Link href="/gallery">
-            <button className="draw meet">Galeria</button>
-          </Link>
-        </div>
-      </main>
+      <div className="container">
+        <main>
+          <h2 className="m-0">Desde 2004</h2>
+          <h1 className="title">Simbiosis</h1>
+          <h2 className="mb-4">peluquería y estética</h2>
+          <p className="fs-4 text-center w-75">
+            <span className="highlight">Simbiosis</span> es un centro de
+            estética unisex supervisado por sus dueños y atendido por estilistas
+            profesionales que utilizan los mejores productos del mercado
+            internacional para el cuidado de tu belleza integral. Acercate o
+            encontranos en nuestras redes sociales para conocer las novedades
+            que nuestro equipo tiene para ofrecerte. Te esperamos!
+          </p>
+          <div className="button-sections d-none d-md-block">
+            <button className="draw meet">Servicios</button>
+            <Link href="/contact">
+              <button className="draw meet">Contacto</button>
+            </Link>
+            <Link href="/gallery">
+              <button className="draw meet">Galeria</button>
+            </Link>
+          </div>
+        </main>
+      </div>
     </Layout>
   );
 }
